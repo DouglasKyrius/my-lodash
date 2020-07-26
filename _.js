36cad176fded76e2f39d171eeaffb399945a4179
DouglasKyrius <douglaskyrius@gmail.com>
2020-07-26 19:46:10 -0300

    add inRange method

--- _.js
+++ _.js
@@ -4,6 +4,22 @@ const _ = {
     const clampedValue = Math.min(lowerClampedValue, upper);
 
     return clampedValue;
+  },
+
+  inRange: function(number, start, end) {
+    if(!end) {
+      end = start;
+      start = 0;
+    }
+    if(start > end) {
+      let tempEnd = end;
+      end = start;
+      start = tempEnd;
+    }
+    
+    const isInRange = start <= number && number < end;
+    
+    return isInRange;
   }
 };
 
